691908a0f12818a878c9049327c4e8829e09ba3a
tusharch0 <tusharchoudhary0003@gmail.com>
2022-09-15 14:48:46 +0530

    setting fix in v2.0

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,12 +1,12 @@
 from .base import *
 
-EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_USE_TLS = True
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = "info@real-estate.com"
+DEFAULT_FROM_EMAIL = "info@real_estate.com"
 DOMAIN = env("DOMAIN")
 SITE_NAME = "Real Estate"
 
