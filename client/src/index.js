56812e6d273b54940cd229dc08715e2b91fc5054
tusharch0 <tusharchoudhary0003@gmail.com>
2022-09-27 15:12:03 +0530

    inital redux loading

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from "./store"
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store ={store}>
-    <App />
-  </Provider>
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
-
-reportWebVitals();
